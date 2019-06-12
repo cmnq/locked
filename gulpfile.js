6ad5b9c79e9aaec0d1b1f74aeec53c6a719ded41
cmnq <kpodufalski@gmail.com>
2019-06-12 13:57:24 +0200

    added prefixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,40 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync').create();
+
+var gulp = require("gulp"),
+sass = require("gulp-sass"),
+postcss = require("gulp-postcss"),
+autoprefixer = require("gulp-autoprefixer"),
+cssnano = require("cssnano"),
+sourcemaps = require("gulp-sourcemaps"),
+browserSync = require("browser-sync").create();
 
 // compile scss into css
 
-function style(){
-    // 1. where is my scss file?
-    return gulp.src('./sass/**/*.scss') // **/* -> this will searching in every folder into sass, which have .scss 
-    // 2. pass that file through sass compiler
+function style() {
+    return gulp
+        .src('./sass/**/*.scss')
+        // Initialize sourcemaps before compilation starts
+        .pipe(sourcemaps.init())
         .pipe(sass())
-    // 3. where do i save the compiled css?
+        .on("error", sass.logError)
+        // Use postcss with autoprefixer and compress the compiled file using cssnano
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        // Now add/write the sourcemaps
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('./css'))
-
-    // 4. stream changes to all browsers
+        // Add browsersync stream pipe after compilation
         .pipe(browserSync.stream());
-
-};
+}
+var paths = {
+    styles: {
+        // By using styles/**/*.sass we're telling gulp to check all folders for any sass file
+        src: "./sass/**/*.scss",
+        // Compiled files will end up in whichever folder it's found in (partials are not compiled)
+        dest: "css/"
+    }
+}
 
 function watch(){
     browserSync.init({
